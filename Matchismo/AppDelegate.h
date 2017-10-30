2b7c3eab4876e4e1d83e6467bc6bef438bcb00b2
Albert <albert@alberts-mac.local>
2017-10-30 09:21:51 +0100

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Albert on 30/10/17.
+//  Copyright © 2017 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Albert on 30/10/17.
+//  Copyright © 2017 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
